f8e528e7c81dbd22acc3e58eec6f12e1a4a71dce
Wellerson <rodrigues3442@hotmail.com>
2023-09-02 20:57:52 -0300

    first commit

---
+++ source/vulkan-engine.cpp
@@ -0,0 +1,12 @@
+﻿// vulkan-engine.cpp: define o ponto de entrada para o aplicativo.
+//
+
+#include "vulkan-engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
